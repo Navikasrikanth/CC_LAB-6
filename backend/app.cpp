f962e7c5c1fda4b094ec2958609a0c49b2a05f3c
Navikasrikanth <navikasrikanth@gmail.com>
2026-02-26 14:38:05 +0530

    Add basic HTTP server in C++
    Implement a simple HTTP server that responds with a greeting and CI/CD information.

--- backend/app.cpp
+++ backend/app.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    const char* response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "<html><body>"
+        "<h1>Hello from C++ Backend!</h1>"
+        "<p>CI/CD Pipeline with Jenkins and NGINX</p>"
+        "<p>Student: PES1UG23CS393</p>"
+        "</body></html>";
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8081);
+
+    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    std::cout << "Server running on port 8081..." << std::endl;
+
+    while (true) {
+        new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+        send(new_socket, response, strlen(response), 0);
+        close(new_socket);
+    }
+
+    return 0;
+}
